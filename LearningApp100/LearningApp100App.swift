36428fa8a6d091ad027b24a8da06ed44a2797a81
Harrison Carroll <harrison_2000@live.co.uk>
2022-10-23 15:54:15 +0100

    Initial Commit

---
+++ LearningApp100/LearningApp100App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp100App.swift
+//  LearningApp100
+//
+//  Created by Harrison Carroll on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
